b15603580d455832ac35765841802ef8f847fa73
Ibrohim <bii23.uz@gmail.com>
2022-10-21 18:36:32 +0500

    Deploy Django Vercel.app

--- core/settings.py
+++ core/settings.py
@@ -127,10 +127,10 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
